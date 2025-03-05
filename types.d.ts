231de114df1cc3a0d11328cefb0f69ee03d83011
Nick Farina <nfarina@gmail.com>
2025-03-05 15:47:38 -0800

    Update tsconfig

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,10 @@
-// This allows TypeScript to assume these libraries are globally available
-// for the whole project.
-// https://bun.sh/docs/typescript#dom-types
-/// <reference lib="dom" />
-/// <reference lib="dom.iterable" />
+// This allows TypeScript to understand what Vite will do when non-code
+// assets are imported.
+declare module "*.woff2" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
